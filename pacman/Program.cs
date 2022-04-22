4ac423c2132bc88cfabe0e299e260d0630abd1ee
Amar Zaidi <76910336+syed-amar-zaidi@users.noreply.github.com>
2022-04-22 08:00:19 -0700

    Updated

--- pacman/Program.cs
+++ pacman/Program.cs
@@ -13,11 +13,12 @@ namespace pacman
     {
         static void Main(string[] args)
         {
+            //Math.Floor function to decide objects to move
             string path = "C:\\Users\\kali\\Documents\\Visual Studio 2015\\Projects\\pacman\\pacman\\files\\maze.txt";
             Grid mazeGrid = new Grid(24, 71, path);
             Pacman player = new Pacman(5, 10, mazeGrid);
             Ghost ghost1 = new Ghost(10, 15, 'H', "left", 0.1f, ' ', mazeGrid);
-            player.draw();
+            mazeGrid.draw();
             player.draw();
             ghost1.draw();
             Console.ReadKey();
